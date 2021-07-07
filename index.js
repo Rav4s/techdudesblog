58eef8f1053779a8a65921a42c95f6b79e41614e
softwaresat <ag.satvik@gmail.com>
2021-07-07 18:42:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -184,12 +184,12 @@ app.get('/login', (req,res)=>
 	});
 // app.listen(80, () => console.log('Server is live on port 80!'))
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
+// httpsServer.listen(443, () => {
+// 	console.log('HTTPS Server running on port 443');
+// });
